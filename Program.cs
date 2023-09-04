6bac861284d75c1ff5c0db50c3a2bc769ba9caab
Kimbill <ayooluwaa@decagonhq.com>
2023-09-04 21:27:05 +0100

    sixth commit

--- Program.cs
+++ Program.cs
@@ -1,31 +1,48 @@
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using RenderSample.Data;
 using RenderSample.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
+var config = builder.Configuration;
+var services = builder.Services;
+var env = builder.Environment;
 
 // Add services to the container.
+builder.Services.AddHttpClient();
+
+//For Entity Framework
+
+builder.Services.AddDbContextAndConfigurations(env, config);
+AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+
+
+builder.Services.AddAuthentication();
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddDbContextAndConfigurations(builder.Environment, builder.Configuration);
-AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-//builder.Services.AddDbContext<SampleContext>(option =>
-//option.UseNpgsql(builder.Configuration.GetConnectionString()));
+
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors();
+;
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
+    app.UseDeveloperExceptionPage();
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+
+app.UseCors(builder => { builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader(); }); // other middleware }
+
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
